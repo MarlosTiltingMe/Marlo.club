0804d63fdd216e6a742fe6454f8f511399e728c7
Carter <csmith@izeni.com>
2016-02-26 11:49:42 -0700

    Polishing user stuff

--- profiles/views.py
+++ profiles/views.py
@@ -8,32 +8,22 @@ from django.conf import settings
 from .models import Name, Posts
 # Create your views here.
 
-def add(request):
-    if request.method == "GET":
-        return render(request, 'profiles/add.html', {'damn': 'daniel'})
-    elif request.method == "POST":
-        reddit_input = pk=request.POST['reddit']
-        name_input = pk=request.POST['name']
-        picture_input = pk=request.POST['picture']
-        try:
-            obj = Name.objects.get(name_text=name_input)
-        except MultipleObjectsReturned and Name.DoesNotExist:
-            obj = Name(name_text=name_input, reddit=reddit_input, picture=picture_input)
-            obj.save()
-            return render(request, 'profiles/add.html', {'was_created': 'You\'ve added your profile! Thanks!'})
-        return render(request, 'profiles/add.html', {'was_created': 'Already exists!'})
+
 
 def post(request):
-    if request.method == "GET":
-        return render(request, 'profiles/post.html')
-    elif request.method == "POST":
-        title_input = pk=request.POST['title']
-        author_input = pk=request.POST['author']
-        body_input = pk=request.POST['body']
-        img_input = pk=request.POST['img']
-        obj = Posts(title_text=title_input, author=author_input, body_text=body_input, img=img_input)
-        obj.save()
-        return render(request, 'profiles/post.html', {'was_posted': 'Thanks for your post!'})
+    if not request.user.is_authenticated():
+        return render(request, 'profiles/login.html')
+    else:
+        if request.method == "GET":
+            return render(request, 'profiles/post.html', {'name': request.user})
+        elif request.method == "POST":
+            title_input = pk=request.POST['title']
+            author_input = request.user
+            body_input = pk=request.POST['body']
+            img_input = pk=request.POST['img']
+            obj = Posts(title_text=title_input, author=author_input, body_text=body_input, img=img_input)
+            obj.save()
+            return render(request, 'profiles/post.html', {'was_posted': 'Thanks for your post!', 'name': request.user})
 
 def postText(request):
     if not request.user.is_authenticated():
@@ -43,24 +33,25 @@ def postText(request):
             return render(request, 'profiles/postText.html')
         elif request.method == "POST":
             title_input = pk=request.POST['title']
-            author_input = pk=request.POST['author']
+            author_input = request.user
             body_input = pk=request.POST['body']
             logs_input = pk=request.POST['logs']
             obj = Posts(title_text=title_input, author=author_input, body_text=body_input, log_text=logs_input)
             obj.save()
-            return render(request, 'profiles/postText.html', {'was_posted': 'Thanks for your post!'})
+            return render(request, 'profiles/postText.html', {'was_posted': 'Thanks for your post!', 'name': request.user})
 
 def posts(request):
     latest_post_list = Posts.objects.all()
     template = loader.get_template('profiles/index.html')
     context = {
-        'all_posts': latest_post_list
+        'all_posts': latest_post_list,
+        'name': request.user,
     }
     return HttpResponse(template.render(context, request))
 
 def thread(request, thread_name):
     get_thread = Posts.objects.filter(title_text=thread_name)
-    return render(request, 'profiles/thread.html', {'thread': get_thread})
+    return render(request, 'profiles/thread.html', {'thread': get_thread, 'name': request.user})
 
 
 #User stuff
@@ -69,24 +60,24 @@ def register(request):
 
 def registerUser(request):
     if request.method == "GET":
-        return render(request, 'profiles/index.html')
+        return render(request, 'profiles/index.html', {'name': request.user})
     elif request.method == "POST":
         uname_input = pk=request.POST['name']
         email_input = pk=request.POST['email']
         password_input = pk=request.POST['password']
         user = User.objects.create_user(uname_input, email_input, password_input)
         user.save()
-        return render(request, 'profiles/register.html', {'was_registered': 'Thanks for your registration!'})
+        return render(request, 'profiles/register.html', {'was_registered': 'Thanks for your registration!', 'name': request.user})
 
 def signin(request):
     if not request.user.is_authenticated():
-        return render(request, 'profiles/login.html')
+        return render(request, 'profiles/login.html', {'name': request.user})
     else:
-        return render(request, 'profiles/index.html')
+        return render(request, 'profiles/index.html', {'name': request.user})
 
 def loginUser(request):
     if request.method == "GET":
-        return render(request, 'profiles/index.html')
+        return render(request, 'profiles/index.html', {'name': request.user})
     elif request.method == "POST":
         uname_input = request.POST['username']
         password_input = request.POST['password']
@@ -94,11 +85,11 @@ def loginUser(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                return render(request, 'profiles/login.html', {'was_logged': 'Thanks for logging in!'})
+                return render(request, 'profiles/login.html', {'was_logged': 'Thanks for logging in!', 'name': request.user})
             else:
-                return render(request, 'profiles/login.html', {'was_logged': 'You\'ve been banned.'})
+                return render(request, 'profiles/login.html', {'was_logged': 'You\'ve been banned.', 'name': request.user})
         else:
-            return render(request, 'profiles/login.html', {'was_logged': 'That, my friend, is an incorrect login.'})
+            return render(request, 'profiles/login.html', {'was_logged': 'That, my friend, is an incorrect login.', 'name': request.user})
 
 def logout_view(request):
     logout(request)
